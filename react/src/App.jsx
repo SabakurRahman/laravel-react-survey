93a67069d72659d3e07499be95073ed33f7e77f2
SabakurRahman <sabakur12360@gmail.com>
2023-12-12 15:41:46 +0600

    signup login in react

---
+++ react/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import { BeakerIcon } from '@heroicons/react/24/solid'
+import './App.css'
+import {RouterProvider} from "react-router-dom";
+
+function App() {
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
---
+++ react/src/router.jsx
@@ -0,0 +1,28 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import Dashboard from "./view/Dashboard.jsx";
+import Surveys from "./view/Surveys.jsx";
+import Login from "./view/Login.jsx";
+import Signup from "./view/Signup.jsx";
+import GuestLayout from "./component/GuestLayout.jsx";
+import DefaultLayout from "./component/DefaultLaout.jsx";
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <DefaultLayout />,
+        children: [
+            { path: "/dashboard", element: <Dashboard /> },
+            { path: "/surveys", element: <Surveys /> },
+        ],
+    },
+    {
+        path: "/",
+        element: <GuestLayout />,
+        children: [
+            { path: "login", element: <Login /> },
+            { path: "signup", element: <Signup /> },
+        ],
+    },
+]);
+
+export default router;
